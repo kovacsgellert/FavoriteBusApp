29d4256e90b96b03c027d4abe711001fd75d73df
Gellért Kovács <kovacsgellertpal@gmail.com>
2025-05-23 20:05:09 +0300

    Remove old client app

--- src/favoritebusapp-client/src/app/constants.ts
+++
@@ -1,5 +0,0 @@
-// the env variable services__api__http__0 is set by .NET Aspire
-export const API_URL =
-  typeof window === "undefined"
-    ? `${process.env.services__api__http__0}/api`
-    : "/api";
